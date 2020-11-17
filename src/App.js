8356e6d8558fae0af3cc14465d4f7271d94d44f8
JQ <2016quezadaj@gmail.com>
2020-11-16 21:03:38 -0800

    Added Jumbotron to HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return(
     <div>
       <h1 className="text-success">RESCORED</h1>
+      <HomePage />
     </div>
   ) 
 }
