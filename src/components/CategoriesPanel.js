d06297770b875f8f5c9465ad5f22b7c87c0de015
JQ <2016quezadaj@gmail.com>
2020-11-18 03:36:23 -0800

    Updated all buttons inside of CategoriesPanel component to display outline styling

--- src/components/CategoriesPanel.js
+++ src/components/CategoriesPanel.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 
 const CategoriesPanel = () => {
     return (
         <div className="categories-panel my-4">
-            <Button variant="success">Success</Button>{' '}
-            <Button variant="warning">Warning</Button>{' '}
-            <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-            <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-            <Button variant="link">Link</Button>
+            <Button variant="outline-warning">Fair Credit: </Button>{' '}
+            <Button variant="outline-danger">Bad Credit: </Button> <Button variant="outline-info">Limited Credit</Button>{' '}
         </div>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,12 +17,11 @@ const HomePage = () => {
                         <h1 className="text-wrap text-truncate text-success">
                             Rebuild your Credit Score
                         </h1>
-                        <CategoriesPanel />
                         <p className="text-wrap text-truncate">
                             This is a modified jumbotron that occupies the entire horizontal space of
                             its parent.
                         </p>
-                        
+                        <CategoriesPanel />
                         </Container>
                     </Jumbotron>
                     </Col>
