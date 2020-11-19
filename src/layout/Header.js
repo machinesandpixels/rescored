c356f5bf4ecdac5a68a70fe759d72c1eb10f901c
JQ <2016quezadaj@gmail.com>
2020-11-19 01:36:56 -0800

    Made some arrangement changes to Navbar links

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -9,32 +9,29 @@ const Header = () => {
     return (
         <Fragment>
             <Navbar className="py-5">
-                <Nav className="m-auto">
-                <Nav.Link href="#home">
-                     Credit Cards
-                </Nav.Link>
-                </Nav>
                 <Navbar.Brand href="#home">
                     <h2 className="ml-5 text-success">
                         RESCORED
                     </h2>
                 </Navbar.Brand>
-                <Nav className="m-auto">
-                <NavDropdown title="Secured Cards" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-                </Nav>
+                <Nav className="ml-auto">
+                <NavDropdown title="Credit Cards" id="collasible-nav-dropdown" className="mr-3">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
 
-                <Nav>
-            
-        
-                
-              
+                <NavDropdown title="Secured Cards" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
                 </Nav>
+                <Nav className="ml-5" />
             </Navbar>
         </Fragment>
     )
