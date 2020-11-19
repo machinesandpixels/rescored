1b37201b6adb8801abfb8e2db3ae31c53e2ff424
JQ <2016quezadaj@gmail.com>
2020-11-19 01:18:01 -0800

    Removed JSX spacing from CategoriesPanel and added margin right  instead

--- src/components/CategoriesPanel.js
+++ src/components/CategoriesPanel.js
@@ -4,15 +4,15 @@ import Button from 'react-bootstrap/Button';
 const CategoriesPanel = () => {
     return (
         <div className="categories-panel my-4">
-            <Button variant="outline-warning">
+            <Button variant="outline-warning" className="mr-3">
                 Fair Credit: 
-            </Button>{' '}
-            <Button variant="outline-danger">
+            </Button>
+            <Button variant="outline-danger" className="mr-3">
                 Bad Credit: 
             </Button> 
             <Button variant="outline-info">
                 Limited Credit
-            </Button>{' '}
+            </Button>
         </div>
     )
 }
