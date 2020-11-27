a276e418d757dad0ea72f8c74891a156bec20835
JQ <2016quezadaj@gmail.com>
2020-11-26 23:56:34 -0800

    Worked on setting up routes to home and /cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch 
+} from 'react-router-dom';
 import Header from '../layout/Header';
 import HomePage from '../pages/HomePage';
+import Cards from '../components/Cards';
 
 const App = () => {
   return(
     <div>
-      <Header />
-      <HomePage />
+      <Router>
+        {/* <Switch> */}
+          <Header />
+          <Route path="/" component={ HomePage } exact />
+          <Route path="/cards" component={ Cards } />
+        {/* </Switch> */}
+      </Router>
     </div>
   ) 
 }
--- src/components/CategoriesPanel.js
+++ src/components/CategoriesPanel.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 const CategoriesPanel = () => {
     return (
         <div className="my-4">
-            <Button variant="outline-warning" className="mr-3">
-                Fair Credit: 689 - 630
-            </Button>
+            
+            <Link to="/cards">
+                <Button variant="outline-warning" className="mr-3">
+                    Fair Credit: 689 - 630 
+                </Button>
+            </Link>
+            
             <Button variant="outline-danger" className="mr-3">
                 Bad Credit: 620 - 350
             </Button> 
