18d801d53d8344eeee6ce86e67b057d34b9d846e
JQ <2016quezadaj@gmail.com>
2021-03-10 21:40:29 -0800

    Created footer component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../layout/Footer';
 
 const Cards = () => {
     return (
         <div>
             <p>Cards</p>
+            <Footer />
         </div>
     )
 }
