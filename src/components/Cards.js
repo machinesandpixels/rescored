0aa9a395fe5eb9cc525f1a63fc636b2781ee638c
JQ <2016quezadaj@gmail.com>
2021-03-14 23:53:13 -0700

    Added back button to Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Footer from '../layout/Footer';
 
 const Cards = () => {
     return (
         <div>
+            <Button variant="outline-success"> Back </Button>
             <p>Cards</p>
             <Footer />
         </div>
