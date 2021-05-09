23ca9156775b38ff370286dc24b85677977428c3
JQ <2016quezadaj@gmail.com>
2021-05-08 17:14:36 -0700

    Added dropdown--link CSS class to remaining Nav links

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -43,11 +43,11 @@ const Header = () => {
                     }  
                     id="collasible-nav-dropdown"
                 >
-                    <NavDropdown.Item className="text-success" href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item className="text-success" href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item className="text-success" href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Item className="text-success dropdown--link" href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item className="text-success dropdown--link" href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item className="text-success dropdown--link" href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item className="text-success" href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown.Item className="text-success dropdown--link" href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
                 </Nav>
                 
