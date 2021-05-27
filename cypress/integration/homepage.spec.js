237119c2755c7e378901b7013010b3f356431636
J <2016quezadaj@gmail.com>
2021-05-26 19:45:16 -0700

    created two basic HomePage test cases

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,10 @@
+describe("Renders HomePage component", () => {
+    it("...Render successful", () => {
+        cy.visit("/");
+    });
+
+    it("...Checks H1 and its content", () => {
+        cy.get('h1').should("exist");
+        cy.get('h1').contains("Rebuild your Credit Score");
+    });
+});
