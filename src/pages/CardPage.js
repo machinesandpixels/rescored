46f31bd488de613f8af17803cb6cd7042646e61d
J <2016quezadaj@gmail.com>
2021-06-11 22:15:08 -0700

    Added Container, Row, Col to CardPage

--- src/pages/CardPage.js
+++ src/pages/CardPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Footer from '../layout/Footer';
 
 const CardPage = (props) => {
     
@@ -7,8 +14,27 @@ const CardPage = (props) => {
     
     return (
         <div>
-            CARD PAGE
-            { card ? console.log(card.name) : <Redirect to='/'  /> }
+            { card ? 
+            <Container fluid>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                    <Card key={card.id} className="my-3 p-3 rounded" style={{ width: '30rem' }}>
+                        <Card.Img variant="top" src={`/${card.image}`} />
+                        <Card.Body>
+                            <Card.Title>{ card.name }</Card.Title>
+                            <Card.Header>Annual Fee</Card.Header>
+                            <Card.Header>${ card.annualFee }</Card.Header>
+                            <Card.Header className="mt-3">APR</Card.Header>
+                            <Card.Header>{ card.apr }%</Card.Header>
+                            <Button href={card.link} className="d-flex      justify-content-center mt-3" variant="outline-success">
+                                Apply
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            </Container> : <Redirect to='/'  /> 
+            }
         </div>
     )
 }
